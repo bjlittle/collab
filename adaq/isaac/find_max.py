cd5838434f7e85fe8422a979600a0ab27a6c7364
Bill Little <bill.james.little@gmail.com>
2024-11-14 16:24:50 +0000

    isaac find-max

---
+++ adaq/isaac/find_max.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import iris
+import numpy as np
+
+
+fname = ("fukushima_grid1_201103.nc")
+cube = iris.load_cube(fname)
+vmin, vmax = np.nan, np.nan
+
+breakpoint()
+
+for i, part in enumerate(cube.slices_over("time")):
+    data = part.data
+    dmin = np.min(data)
+    dmax = np.max(data)
+    print(f"\t{i=} {dmax=}")
+    if np.isnan(vmin):
+        vmin = dmin
+    elif dmin < vmin:
+        vmin = dmin
+    if np.isnan(vmax):
+        vmax = dmax
+    elif dmax > vmax:
+        vmax = dmax
+
+print(f"{vmin=}, {vmax=}")
