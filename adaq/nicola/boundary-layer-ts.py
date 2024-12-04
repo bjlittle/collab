a6d2485cf6f22abca17b9e0cfb349b5f5ba52ec4
Bill Little <bill.james.little@gmail.com>
2024-12-04 15:58:01 +0000

    bl vars

--- adaq/nicola/boundary-layer-ts.py
+++ adaq/nicola/boundary-layer-ts.py
@@ -25,10 +25,11 @@ import pyvista as pv
 def callback() -> None:
     global tstep
     global n_tsteps
-    global t
-    global cmap
-    global clim
-    global mesh
+    global bl_t
+    global bl_data
+    global bl_mesh
+    global bl_cmap
+    global bl_clim
     global actor_text
     global actor_scalar
     global factor
@@ -39,65 +40,63 @@ def callback() -> None:
     # if tstep == 0:
         # exit()
 
-    dt = t.units.num2date(t.points[tstep])
+    dt = bl_t.units.num2date(bl_t.points[tstep])
 
-    # mesh["data"] = data[tstep][:].flatten()
-    # mesh.save(f"vtk/mesh_boundary_layer_{dt.strftime("%Y%m%d%H%M")}.vtk")
-    mesh = pv.read(f"vtk/mesh_boundary_layer_{dt.strftime('%Y%m%d%H%M')}.vtk")
-    mesh = mesh.cell_data_to_point_data()
-    mesh.compute_normals(cell_normals=False, point_normals=True, inplace=True, flip_normals=True)
-    mesh.warp_by_scalar(scalars="data", inplace=True, factor=factor)
-    # mesh.smooth_taubin(inplace=True)
-    mesh.active_scalars_name = "data"
+    # bl_mesh["data"] = bl_data[tstep][:].flatten()
+    # bl_mesh.save(f"vtk/mesh_boundary_layer_{dt.strftime("%Y%m%d%H%M")}.vtk")
+    bl_mesh = pv.read(f"vtk/mesh_boundary_layer_{dt.strftime('%Y%m%d%H%M')}.vtk")
+    bl_mesh = bl_mesh.cell_data_to_point_data()
+    bl_mesh.compute_normals(cell_normals=False, point_normals=True, inplace=True, flip_normals=True)
+    bl_mesh.warp_by_scalar(scalars="data", inplace=True, factor=factor)
+    # bl_mesh.smooth_taubin(inplace=True, normalize_coordinates=True)
+    bl_mesh.active_scalars_name = "data"
 
-    p.add_mesh(mesh, name="boundary", opacity=opacity, cmap=cmap, clim=clim, render=False, reset_camera=False, show_scalar_bar=False)
+    p.add_mesh(bl_mesh, name="boundary", opacity=opacity, cmap=bl_cmap, clim=bl_clim, render=False, reset_camera=False, show_scalar_bar=False)
     p.add_actor(actor_scalar)
 
     text = f"{dt.strftime(fmt)}"
     actor_text.SetText(3, text)
 
 
-fname = "data/Boundary_Layer_C1.nc"
-cube = iris.load_cube(fname)
+bl_fname = "data/Boundary_Layer_C1.nc"
+bl_cube = iris.load_cube(bl_fname)
 
 # bootstrap
-t = cube.coord("time")
-y = cube.coord("latitude")
-x = cube.coord("longitude")
+bl_t = bl_cube.coord("time")
+bl_y = bl_cube.coord("latitude")
+bl_x = bl_cube.coord("longitude")
 
 fmt = "%Y-%m-%d %H:%M"
 
-ds = nc.Dataset(fname)
-data = ds.variables["var__boundary_layer_depth"]
+bl_ds = nc.Dataset(bl_fname)
+bl_data = bl_ds.variables["var__boundary_layer_depth"]
 
-n_tsteps = t.shape[0]
+n_tsteps = bl_t.shape[0]
 tstep = 0
 
-y_cb = y.contiguous_bounds()
-x_cb = x.contiguous_bounds()
+y_cb = bl_y.contiguous_bounds()
+x_cb = bl_x.contiguous_bounds()
 
 threshold = None
 vmin = threshold if threshold else 50.0
 vmax = 640.0  # 639.9715
-clim = (vmin, vmax)
+bl_clim = (vmin, vmax)
 
-# cmap = "fire_r"
-# cmap = "balance"
-cmap = "deep_r"
+bl_cmap = "deep_r"
 color = "white"
 
 factor = 4e-6
 
-# mesh = geovista.Transform.from_1d(x_cb, y_cb)
-# mesh["data"] = data[tstep][:].flatten()
+# bl_mesh = geovista.Transform.from_1d(x_cb, y_cb)
+# bl_mesh["data"] = bl_data[tstep][:].flatten()
 
-dt = t.units.num2date(t.points[tstep])
-# mesh.save(f"vtk/mesh_boundary_layer_{dt.strftime("%Y%m%d%H%M")}.vtk")
-mesh = pv.read(f"vtk/mesh_boundary_layer_{dt.strftime('%Y%m%d%H%M')}.vtk")
-mesh = mesh.cell_data_to_point_data()
-mesh.compute_normals(cell_normals=False, point_normals=True, inplace=True, flip_normals=True)
-mesh.warp_by_scalar(scalars="data", inplace=True, factor=factor)
-# mesh.smooth_taubin(inplace=True)
+dt = bl_t.units.num2date(bl_t.points[tstep])
+# bl_mesh.save(f"vtk/mesh_boundary_layer_{dt.strftime("%Y%m%d%H%M")}.vtk")
+bl_mesh = pv.read(f"vtk/mesh_boundary_layer_{dt.strftime('%Y%m%d%H%M')}.vtk")
+bl_mesh = bl_mesh.cell_data_to_point_data()
+bl_mesh.compute_normals(cell_normals=False, point_normals=True, inplace=True, flip_normals=True)
+bl_mesh.warp_by_scalar(scalars="data", inplace=True, factor=factor)
+# bl_mesh.smooth_taubin(inplace=True, normalize_coordinates=True)
 
 bbox = BBox([-1.0, 0.9, 0.9, -1.0], [52.9, 52.9, 51.5, 51.5])    # [52.7, 52.7, 51.7, 51.7])
 grid = regular_grid("r1000")
@@ -112,10 +111,10 @@ p.enable_lightkit()
 p.set_background(color="black")
 
 opacity = None
-actor_boundary = p.add_mesh(mesh, name="boundary", opacity=opacity, cmap=cmap, clim=clim, show_edges=False, show_scalar_bar=False)
+actor_boundary = p.add_mesh(bl_mesh, name="boundary", opacity=opacity, cmap=bl_cmap, clim=bl_clim, show_edges=False, show_scalar_bar=False)
 p.view_poi()
 
-sargs = {"color": color, "title": f"{capitalise(cube.name())} / {str(cube.units)}", "font_family": "arial", "label_font_size": 10, "n_labels": 5}
+sargs = {"color": color, "title": f"{capitalise(bl_cube.name())} / {str(bl_cube.units)}", "font_family": "arial", "label_font_size": 10, "n_labels": 5}
 actor_scalar = p.add_scalar_bar(mapper=actor_boundary.mapper, **sargs)
 
 p.add_mesh(base, texture=texture, opacity=0.5, show_edges=True)
@@ -136,7 +135,7 @@ p.add_text(
 )
 p.add_axes(color=color)
 
-del cube
+del bl_cube
 
 input()
 
