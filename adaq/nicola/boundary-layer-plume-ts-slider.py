93e42ad472ae8835e41275075b1837d5e24c5d76
Bill Little <bill.james.little@gmail.com>
2024-12-06 19:43:53 +0000

    add checkboxes

--- adaq/nicola/boundary-layer-plume-ts-slider.py
+++ adaq/nicola/boundary-layer-plume-ts-slider.py
@@ -1,15 +1,11 @@
 """Execute script with 'python -i <script>'."""
 
-from datetime import datetime
 from functools import partial
-from pathlib import Path
 
-from cf_units import Unit
 import geovista
 import geovista.theme
 from geovista.core import add_texture_coords
 from geovista.geodesic import BBox
-from geovista.geometry import coastlines
 from geovista.pantry.meshes import regular_grid
 from geovista.pantry.data import capitalise
 from geovista.qt import GeoBackgroundPlotter
@@ -19,10 +15,19 @@ import iris
 import netCDF4 as nc
 import numpy as np
 import pyvista as pv
+from pyvista import Actor
+
+
+show_plume = True
+show_boundary = True
+show_contour = False
+isosurfaces = 200
+isosurfaces_range = (None, None)
+render = True
 
 
 # def callback(value) -> None:
-def callback(value: None) -> None:
+def callback_render(value: None) -> None:
     global tstep
     global n_tsteps
     global bl_t
@@ -36,10 +41,15 @@ def callback(value: None) -> None:
     global p_clim
     global actor_text
     global actor_scalar
+    global actor_plume
+    global actor_boundary
     global factor
-    global opacity
+    global render
 
-    value = int(value)
+    if value is None:
+        value = tstep
+    else:
+        value = int(value)
 
     tstep = value % n_tsteps
     bl_tstep = tstep // 4
@@ -50,31 +60,64 @@ def callback(value: None) -> None:
     dt = p_t.units.num2date(p_t.points[tstep])
     bl_dt = bl_t.units.num2date(bl_t.points[bl_tstep])
 
-    data = np.ma.masked_less_equal(p_data[tstep][:], 0).filled(np.nan).flatten()
-    p_mesh["data"] = data
-    p.add_mesh(p_mesh.threshold(), cmap=p_cmap, clim=p_clim, render=False, reset_camera=False, name="plume",
-               show_scalar_bar=False)
+    if render:
+        if show_plume:
+            data = np.ma.masked_less_equal(p_data[tstep][:], 0).filled(np.nan).flatten()
+            p_mesh["data"] = data
+            actor_plume = p.add_mesh(p_mesh.threshold(), cmap=p_cmap, clim=p_clim, render=False, reset_camera=False, name="plume",
+                       opacity="linear_r", show_scalar_bar=False)
+        else:
+            actor_plume.SetVisibility(False)
 
-    # bl_mesh["data"] = bl_data[bl_tstep][:].flatten()
-    # bl_mesh.save(f"vtk/mesh_boundary_layer_{bl_dt.strftime("%Y%m%d%H%M")}.vtk")
-    bl_mesh = pv.read(f"vtk/mesh_boundary_layer_{bl_dt.strftime('%Y%m%d%H%M')}.vtk")
-    bl_mesh = bl_mesh.cell_data_to_point_data()
-    bl_mesh.compute_normals(cell_normals=False, point_normals=True, inplace=True, flip_normals=True)
-    bl_mesh.warp_by_scalar(scalars="data", inplace=True, factor=factor)
-    # bl_mesh.smooth_taubin(inplace=True, normalize_coordinates=True)
-    bl_mesh.active_scalars_name = "data"
+    if show_boundary:
+        # bl_mesh["data"] = bl_data[bl_tstep][:].flatten()
+        # bl_mesh.save(f"vtk/mesh_boundary_layer_{bl_dt.strftime("%Y%m%d%H%M")}.vtk")
+        bl_mesh = pv.read(f"vtk/mesh_boundary_layer_{bl_dt.strftime('%Y%m%d%H%M')}.vtk")
+        bl_mesh = bl_mesh.cell_data_to_point_data()
+        bl_mesh.compute_normals(cell_normals=False, point_normals=True, inplace=True, flip_normals=True)
+        bl_mesh.warp_by_scalar(scalars="data", inplace=True, factor=factor)
+        # bl_mesh.smooth_taubin(inplace=True, normalize_coordinates=True)
+        bl_mesh.active_scalars_name = "data"
+        actor_boundary = p.add_mesh(bl_mesh, name="boundary", cmap=bl_cmap, clim=bl_clim, render=False, reset_camera=False, show_scalar_bar=False)
+        p.add_actor(actor_scalar)
+    else:
+        actor_boundary.SetVisibility(False)
 
-    p.add_mesh(bl_mesh, name="boundary", opacity=opacity, cmap=bl_cmap, clim=bl_clim, render=False, reset_camera=False, show_scalar_bar=False)
-    p.add_actor(actor_scalar)
+    render = True
 
     text = f"{dt.strftime(fmt)}"
     actor_text.SetText(3, text)
 
 
+def callback_base(actors: Actor, flag: bool) -> None:
+    global render
+
+    render = False
+    for actor in actors:
+        actor.SetVisibility(flag)
+    callback_render(None)
+
+
+def callback_plume(flag: bool) -> None:
+    global show_plume
+
+    show_plume = bool(flag)
+    callback_render(None)
+
+
+def callback_boundary(flag: bool) -> None:
+    global show_boundary
+    global render
+
+    render = False
+    show_boundary = bool(flag)
+    callback_render(None)
+
+
 bl_fname = "data/Boundary_Layer_C1.nc"
 bl_cube = iris.load_cube(bl_fname)
 
-run = "R2_T2_V2"
+run = "R1_T1_V2"
 p_fname = f"data/{run}/Fields_grid3_C1.nc"
 p_cube = iris.load_cube(p_fname)
 
@@ -92,7 +135,9 @@ vmax = 640.0  # 639.9715
 bl_clim = (vmin, vmax)
 bl_cmap = "deep_r"
 p_cmap = "fire_r"
-p_clim = (0.0, 9e-6)
+p_clim = (0.0, 9e-6)  # R2_T2_V2
+p_clim = (0.0, 5.7e-5)  # R1_T1_V1
+p_clim = (0.0, 1.6e-5)  # R1_T1_V2
 color = "white"
 factor = 4e-6  # on-change: regenerate plume.vtk !
 
@@ -133,8 +178,6 @@ texture = wrap_texture(geovista.natural_earth_hypsometric())
 outline = p_mesh.extract_feature_edges()
 
 pv.global_theme.allow_empty_mesh = True
-
-
 p = GeoBackgroundPlotter()
 
 # p.enable_anti_aliasing(aa_type="ssaa")
@@ -142,20 +185,20 @@ p.enable_lightkit()
 p.set_background(color="black")
 
 plume = p_mesh.threshold()
-p.add_mesh(plume, name="plume", cmap=p_cmap, clim=p_clim, show_scalar_bar=False)
+actor_plume = p.add_mesh(plume, name="plume", cmap=p_cmap, clim=p_clim, opacity="linear_r", show_scalar_bar=False)
 
-opacity = None
-actor_boundary = p.add_mesh(bl_mesh, name="boundary", opacity=opacity, cmap=bl_cmap, clim=bl_clim, show_edges=False, show_scalar_bar=False)
+actor_boundary = p.add_mesh(bl_mesh, name="boundary", cmap=bl_cmap, clim=bl_clim, show_edges=False, show_scalar_bar=False)
 p.view_poi()
 
 sargs = {"color": color, "title": f"{capitalise(bl_cube.name())} / {str(bl_cube.units)}", "font_family": "arial", "label_font_size": 10, "n_labels": 5}
 actor_scalar = p.add_scalar_bar(mapper=actor_boundary.mapper, **sargs)
 
-p.add_mesh(base, texture=texture, opacity=0.5, show_edges=True)
+actor_base = p.add_mesh(base, texture=texture, opacity=0.3, show_edges=True)
 p.add_mesh(outline, color="orange", line_width=1)
 
 obs = geovista.Transform.from_points(xs=[0.1313, -0.2408], ys=[52.1951, 52.5703])
-p.add_point_labels(obs, ["Cambridge", "Peterborough"], text_color=color, render_points_as_spheres=True, point_color="red", point_size=10, font_size=10, justification_horizontal="right", justification_vertical="top", shape=None)
+actor_obs = p.add_point_labels(obs, ["Cambridge", "Peterborough"], text_color=color, render_points_as_spheres=True, point_color="red", point_size=10, font_size=10, justification_horizontal="right", justification_vertical="top", shape=None)
+actor_pts = list(p.actors.values())[-2]
 
 text = f"{dt.strftime(fmt)}"
 actor_text = p.add_text(
@@ -170,5 +213,60 @@ p.add_text(
 )
 p.add_axes(color=color)
 
-p.add_slider_widget(callback, (0, n_tsteps-1), value=0, pointa=(0.4, 0.9), pointb=(0.95, 0.9), slider_width=0.02, title_height=0.02, tube_width=0.001, fmt="%.0f", color=color, title="Time Step", style="modern")
+size, pad = 20, 3
+x, y = 10, 100
+offset = size * 0.2
+font_size = 5
+p.add_checkbox_button_widget(
+    partial(callback_base, [actor_base, actor_obs, actor_pts]),
+    value=True,
+    color_on="green",
+    color_off="red",
+    size=size,
+    position=(x, y),
+)
+p.add_text(
+    "Base",
+    position=(x + size + offset, y + offset),
+    font_size=font_size,
+    color=color,
+)
+
+y += size + pad
+
+p.add_checkbox_button_widget(
+    callback_plume,
+    value=show_plume,
+    color_on="green",
+    color_off="red",
+    size=size,
+    position=(x, y),
+)
+p.add_text(
+    "Plume",
+    position=(x + size + offset, y + offset),
+    font_size=font_size,
+    color=color,
+)
+
+y += size + pad
+
+p.add_checkbox_button_widget(
+    callback_boundary,
+    value=show_boundary,
+    color_on="green",
+    color_off="red",
+    size=size,
+    position=(x, y),
+)
+p.add_text(
+    "Boundary Layer",
+    position=(x + size + offset, y + offset),
+    font_size=font_size,
+    color=color,
+)
+
+y += size + pad
+
+p.add_slider_widget(callback_render, (0, n_tsteps-1), value=0, pointa=(0.4, 0.9), pointb=(0.95, 0.9), slider_width=0.02, title_height=0.02, tube_width=0.001, fmt="%.0f", color=color, title="Time Step", style="modern")
 p.show()
